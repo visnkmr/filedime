d8a63dba2d0d87ca8d5fe23fbc691e95fb58664e
12core1 <12533753+visnkmr@users.noreply.github.com>
2024-01-31 01:52:21 +0530

    highlights on focus

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "focus:bg-gray-200 focus:dark:bg-gray-700 rounded-lg border bg-card text-card-foreground shadow-sm",
       className
     )}
     {...props}
