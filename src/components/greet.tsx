1f5b54b728e10c603303ccb973548aa419894a63
12core1 <12533753+visnkmr@users.noreply.github.com>
2024-03-03 12:16:59 +0530

    youtube vids also load an empty screen

--- src/components/greet.tsx
+++ src/components/greet.tsx
@@ -1133,6 +1133,8 @@ export default function Greet() {
       
     }
     return (<>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/3ktXIFv35Yo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+
     <button onClick={()=>{
       ws.send("");
     }}>test</button>
