7496e1ca6247e7093da5f9d1a1f0a62ab5005d61
12core1 <12533753+visnkmr@users.noreply.github.com>
2025-05-27 23:49:02 +0530

    fixing tauri config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import Greet from '../components/greet'
 import GPTchatinterface from '../components/gptchatinterface'
 
 export default function Home() {
-  let url=typeof window !== 'undefined' ? window.location.hostname : '/'
-      console.log(url)
-      return <GPTchatinterface fgptendpoint={url} setasollama={true}/>
-  // return (
-  //   <main className="overflow-hidden h-full">
-  //     <Greet />
-  //   </main>
-  // )
+  // let url=typeof window !== 'undefined' ? window.location.hostname : '/'
+  //     console.log(url)
+  //     return <GPTchatinterface fgptendpoint={url} setasollama={true}/>
+  return (
+    <main className="overflow-hidden h-full">
+      <Greet />
+    </main>
+          )
 }
